d101be229e66da1b59f149b927fb8ca2a13d902b
totton <u1_915da3@yahoo.co.jp>
2023-02-18 17:37:19 +0900

    20230218

--- app/index.js
+++ app/index.js
@@ -1,18 +1,58 @@
 const path = require('path');
+const axios = require('axios');
 require('dotenv').config({
 	path: path.resolve(__dirname, '../.env')
 });
 const { Client } = require('@notionhq/client');
 
-const notion = new Client({ auth: process.env.NOTION_KEY })
+const token = process.env.NOTION_KEY
 const databaseId = process.env.NOTION_DATABASE_ID
+const pageId = process.env.NOTION_ITEM_ID
 
-try {
-	const myPage = notion.databases.create({
-		database_id: databaseId,
-	})
-	console.log(myPage);
+// getDatabase(databaseId);
+getPage(pageId);
 
-} catch (error) {
-	console.error(error.body)
-}
+async function getDatabase(databaseId) {
+	const response = await axios({
+		method: 'post',
+		url: 'https://api.notion.com/v1/databases/' + databaseId + '/query',
+		headers: {
+			'Authorization': `Bearer ${token}`,
+			'Notion-Version': '2021-08-16', // 2022年7月時点 必要に応じて変更してください
+			'Content-Type': 'application/json',
+		},
+	}).catch((error) => {
+		//接続に失敗した場合の処理
+		console.log('err', error);
+	});
+	// データ1件以上取得できた場合
+	if (response && response.data) {
+		for (const data of response.data.results) {
+			// 取得したテーブルデータを使った処理を記述
+			console.log('**********************');
+			console.log(data);
+			console.log('**********************');
+		}
+	}
+};
+
+async function getPage(pageId) {
+	const response = await axios({
+		method: 'post',
+		url: 'https://api.notion.com/v1/pages/' + pageId,
+		headers: {
+			'Authorization': `Bearer ${token}`,
+			'Notion-Version': '2021-08-16', // 2022年7月時点 必要に応じて変更してください
+			'Content-Type': 'application/json',
+		},
+	}).catch((error) => {
+		//接続に失敗した場合の処理
+		console.log('err', error);
+	});
+	// データ1件以上取得できた場合
+	if (response && response.data) {
+		console.log('**********************');
+		console.log(response.data);
+		console.log('**********************');
+	}
+};
